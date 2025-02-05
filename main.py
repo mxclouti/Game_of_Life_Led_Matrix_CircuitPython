7a0781a5667cfbaf12669f51c64ddaf3e84f3241
mxclouti <mxclouti@gmail.com>
2025-02-04 20:43:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # Conway's "Game of Life"
 # Martin Cloutier - February 2025
 # mxclouti@gmail.com
-#
+# Led Matrix 64x64 - Hub75e
+# It uses only 1 dimension arrays for speed
+# and memory efficiency.
+# I tried with complex data structures:
+# Too slow and not enough memory.
+# CircuitPython code tested on a Raspberry Pico 2 W.
+# About 3 generations per second.
 
 import board
 import displayio
